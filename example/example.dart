9f982dc3cf19c34929e34b88e41b7c448f96a0e9
Dennis Loose <dennis.loose@canonical.com>
2022-10-05 11:53:43 +0200

    update example

--- example/example.dart
+++ example/example.dart
@@ -1,13 +1,7 @@
-import 'package:dbus/dbus.dart';
 import 'package:ubuntu_session/ubuntu_session.dart';
 
-void main() async {
-  final manager = GnomeSessionManager();
-  await manager.connect();
-  try {
-    await manager.reboot();
-  } on DBusMethodResponseException catch (e) {
-    print('Error: $e');
-  }
-  await manager.close();
+void main() {
+  final session = UbuntuSession();
+  print(session.desktop);
+  session.reboot();
 }
